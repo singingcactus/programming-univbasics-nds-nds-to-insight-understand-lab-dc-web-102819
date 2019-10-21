4741ce971d871736746cf135651dfaa30e366116
Matt Bechtel <bechma02@gmail.com>
2019-10-21 16:11:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  puts directors_database[0][:name]
+  puts directors_database[0][:movies]
   titles = []
   director_index = 0
   i = 0
