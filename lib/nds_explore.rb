035a7bcf19edc834b0bc3e5d69f0ea38519a93bf
Matt Bechtel <bechma02@gmail.com>
2019-10-21 16:09:41 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,23 +11,10 @@ end
 
 def print_first_directors_movie_titles
   puts directors_database[0]
-
   titles = []
   director_index = 0
   i = 0
 
-  while director_index == 0 do
-    movie_index = 1
-    movie_selection = 0
-
-    while movie_selection < directors_database[director_index][movie_index].length do
-        titles[movie_selection] = directors_database[director_index][movie_index][movie_selection]
-        puts "The data at #{director_index}, #{movie_index}, #{movie_selection} is directors_database[director_index][movie_index][movie_selection]"
-        movie_selection += 1
-    end
-
-    director_index += 1
-  end
 
   while i < titles.length do
     puts titles[i]
