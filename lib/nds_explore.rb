83e415b50448fb3d8b65d73bdb73edf8dbd65095
Matt Bechtel <bechma02@gmail.com>
2019-10-21 15:54:15 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,32 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
+  require "pp"
+  pp directors_database
   # Change the code below to pretty print the nds with pp
   nil
 end
 
 def print_first_directors_movie_titles
+  titles = []
+  director_index = 0
+  i = 0
+
+  while director_index == 0 do
+    puts "director's name is #{directors_database[director_index][:name]}"
+    movie_index = 0
+
+    while movie_index < directors_database[director_index].length do
+        titles[movie_index] = directors_database[director_index][movie_index]
+        movie_index += 1
+    end
+
+    director_index += 1
+  end
+
+  while i < titles.length do
+    print titles[i]
+    i += 1
+  end
+
 end
